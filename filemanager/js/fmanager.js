6cfb15cff693a8db730e685a0b95043110ef268b
unknown <alexandrtai@gmail.com>
2015-03-04 15:28:39 +0200

    add failmanager

---
+++ filemanager/js/fmanager.js
@@ -0,0 +1,19 @@
+//fmanager.js
+/*
+	json такого формата 
+	{
+		"fileName" : "name" ,
+		"fileformat" : "txt|html",
+		"fileSize" : 10
+		"fileBody" : ""
+	}
+	
+	
+
+*/
+
+
+//http://www.datatables.net/
+
+
+$("#content").load("1.txt"); 
