7b83b67517ad1d2ff25e66477279183ee13ac716
aitkachuk <alexandrtai@gmail.com>
2015-03-10 23:22:51 +0200

    edit home

--- angular/js/main.js
+++ angular/js/main.js
@@ -5,7 +5,7 @@ var App=angular.module('myshop',[]);
 
 App.controller('iControler',function($scope, $http){
 	
-	$http.get('http://www.assembler.wc.lt/test/').
+	$http.get('http://192.168.56.20/json/').
     success(function(data, status, headers, config) {
       $scope.posts = data;
     }).
