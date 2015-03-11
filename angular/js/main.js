f7dfa5fc5e47ad4c2ac4a57cf9ec501f26840724
aitkachuk <alexandrtai@gmail.com>
2015-03-11 11:16:53 +0200

    edit home 2

--- angular/js/main.js
+++ angular/js/main.js
@@ -1,17 +1,42 @@
 var App=angular.module('myshop',[]);
 
 
-
-
-App.controller('iControler',function($scope, $http){
-	
-	$http.get('http://192.168.56.20/json/').
+App.service('products', function() {
+  /*
+  $http.get('http://192.168.56.20/json/').
     success(function(data, status, headers, config) {
       $scope.posts = data;
     }).
     error(function(data, status, headers, config) {
       // log error
     });
+  */
+  
+	this.getItems= function($http, $scope) { 
+		  var ret;
+		  $http.get('http://192.168.56.20/json/').
+		  success(function(data, status, headers, config) {
+			  $scope.posts = data;
+			}).
+			error(function(data, status, headers, config) {
+			  // log error
+			});
+		
+			
+	
+	}
+	
+  
+});
+
+
+App.controller('iControler',function(products,$http, $scope){
+	
+	
+	
+		
+	products.getItems($http, $scope);
+	
 	
 	
 });
